a8103617668d6727c175207c3dd879c1c1e08617
Zakyyy <73029787+AthaDzaky@users.noreply.github.com>
2022-04-10 21:38:40 +0700

    Update 1.R

--- 1.R
+++ 1.R
@@ -5,3 +5,16 @@ dgeom(3, 0.2)
 #1b mean Distribusi Geometrik dengan 10000 data random , prob = 0,20 dimana distribusi
 #geometrik acak tersebut X = 3 ( distribusi geometrik acak () == 3 )
 mean(rgeom(10000,0.2)==3)
+
+#1c Bandingkan Hasil poin a dan b , apa kesimpulan yang bisa didapatkan?
+
+#1d Histogram Distribusi Geometrik , Peluang X = 3 gagal Sebelum Sukses Pertama
+set.seed(0)
+hist(rgeom(n,p),main="Histogram Binomial Sembuh Covid")
+
+#1e Nilai Rataan (μ) dan Varian (σ²) dari Distribusi Geometrik.
+rataan=(1/p)
+rataan
+
+varians=(1-p)/p^2
+varians
