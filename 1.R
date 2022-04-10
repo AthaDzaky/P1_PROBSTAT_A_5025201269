960714e719077adde97cb24caef35322355ef3e9
Zakyyy <73029787+AthaDzaky@users.noreply.github.com>
2022-04-10 21:10:35 +0700

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,6 +1,5 @@
 #1a Berapa peluang penyurvei bertemu x = 3 orang yang tidak menghadiri acara vaksinasi
 #sebelum keberhasilan pertama ketika p = 0,20 dari populasi menghadiri acara vaksinasi ? (distribusi Geometrik)
-
 dgeom(3, 0.2)
 
 #1b mean Distribusi Geometrik dengan 10000 data random , prob = 0,20 dimana distribusi
